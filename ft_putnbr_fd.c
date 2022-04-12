2583b3fcb0a84da5e6c785110f5f09584f589d6a
Paphawit Ngamchaliew <greatng@Paphawits-MacBook-Air.local>
2022-04-12 20:33:41 +0700

    putnbr upgrade

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pngamcha <pngamcha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pngamcha <pngamcha@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 22:49:13 by pngamcha          #+#    #+#             */
-/*   Updated: 2022/02/18 23:24:20 by pngamcha         ###   ########.fr       */
+/*   Updated: 2022/04/12 20:33:13 by pngamcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,16 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	long int	nbr;
 	char		c;
-	int			div;
 
-	div = 1000000000;
 	nbr = n;
 	if (nbr < 0)
 	{
 		nbr = nbr * (-1);
 		write(fd, "-", 1);
 	}
-	while (!(nbr / div) && div != 1)
-		div = div / 10;
-	while (div)
-	{
-		c = (nbr / div) + 48;
-		write(fd, &c, 1);
-		nbr = nbr % div;
-		div = div / 10;
-	}
+	c = (nbr % 10) + 48;
+	nbr = nbr / 10;
+	if (nbr)
+		ft_putnbr_fd(nbr, fd);
+	write(fd, &c, 1);
 }
